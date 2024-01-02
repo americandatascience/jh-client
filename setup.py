f5cedb3c57e22010b9fd841be33c9cdcfb69b622
aychang95 <aychang995@gmail.com>
2024-01-02 15:59:32 -0500

    Package name

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from setuptools import setup, find_packages  # noqa: H301
 #
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
-NAME = "openapi-client"
-VERSION = "1.0.0"
+NAME = "jh-client"
+VERSION = "1.1.0"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",
